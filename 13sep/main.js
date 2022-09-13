0651d6cd1d19dcb3cd87cb06f8dc7889f6bd9eb9
silvia <silviavergara.14sa@gmail.com>
2022-09-13 14:36:38 -0300

    declaraciones

---
+++ 13sep/main.js
@@ -0,0 +1,26 @@
+
+const numeration = 1311            // var es const 
+const centimeters =1.63
+const left = 4
+const miles = 100
+
+let points     //let cambia en el tiempo el valor ejemplo: el puntaje de un juego va cambiando 
+               // const es algo que se mantendra en la misma cifra siempre
+
+//tipo de datos
+let flag = true // boolean o dicotomica significa verdadero o falso si o no existe o no existe solo son dos opciones
+                // en los numeros nunca se pone entre comillas por que lo leera como texto 
+
+//arrays cada elemento tiene su indice 
+let arr =   [ 1, 5, 6, 9]    //indice es del parte del indice ej: 0 1 2 3 4 5 indice
+                            //valor es los numeros seleccionados: 7 9 4 2 8 4 valor 
+
+
+if (condicion) nos devuelve a un true o false
+else en caso contrario are esto   
+
+
+===: se asegura que el tipo de dato sea texto o numero 
+==: comprar dos cosas sin ser extricto 
+
+
