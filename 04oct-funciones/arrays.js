17ed027d8d86925f7aefce692165992f2989d25a
silvia <silviavergara.14sa@gmail.com>
2022-10-04 14:49:24 -0300

    arreglos invertirlo

---
+++ 04oct-funciones/arrays.js
@@ -0,0 +1,5 @@
+const numbers =   [ 1, 2, 3, 4, 5, 6,]// que pueda agarrar cualquier arreglo e invertirlo
+
+const invertArray = (originalArray){
+    
+}
---
+++ 04oct-funciones/main.js
@@ -0,0 +1,20 @@
+
+const isAdult = (age) => {
+    if (age >= 18) {
+        return true
+    } else {
+        return false
+    }
+}
+const logmessage = (age) => {
+    if (isAdult(age)) {
+        console.log("es mayor de edad")
+    } else {
+        console.log("es menor de edad")
+    }
+}
+const transformAge = (age) => {
+    return Number(age)
+}
+const userAge = prompt("Indicanos tu edad")
+logmessage (transformAge(userAge))
